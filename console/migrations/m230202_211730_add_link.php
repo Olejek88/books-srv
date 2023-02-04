4bd58e8cc43c140b339f961d6127fb0c3130f2a6
oleg.romanov <oleg.romanov@apprime.de>
2023-02-04 13:01:09 +0100

    Change field name

--- console/migrations/m230202_211730_add_link.php
+++ console/migrations/m230202_211730_add_link.php
@@ -15,7 +15,7 @@ class m230202_211730_add_link extends Migration
     public function safeUp()
     {
         $this->addColumn(self::BOOK, 'link', $this->string()->notNull()->defaultValue(""));
-        $this->addColumn(self::BOOK, 'amazon_image', $this->string()->notNull()->defaultValue(""));
+        $this->addColumn(self::BOOK, 'amazonImage', $this->string()->notNull()->defaultValue(""));
     }
 
     /**
